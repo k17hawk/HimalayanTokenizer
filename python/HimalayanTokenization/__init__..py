c76379a7e6e0c093d22a787dc4af2817162ca95c
kq7hawk <kumardahal536@gmail.com>
2026-07-23 18:03:28 -0700

    updated the path for the vocab with the case

--- python/HimalayanTokenization/__init__..py
+++ python/HimalayanTokenization/__init__..py
@@ -1,8 +1,8 @@
-from importlib.resources import files
 from HimalayanTokenization import PyHimalayanTokenization
+from importlib.resources import files
 
 def load_default_tokenizer(mode="LM"):
     tok = PyHimalayanTokenization(mode=mode)
-    vocab_path = files("himalayan_tokenizer").joinpath("vocab.tsv")
+    vocab_path = files(__name__).joinpath("vocab.tsv")
     tok.load_vocab_tsv(str(vocab_path))
     return tok
